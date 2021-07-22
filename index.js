98bb387153c918986f2cc0c9a1cede4cfa33439b
Jarrison Cano <jarrison.cano@hotmail.com>
2021-07-22 17:23:34 -0500

    :heavy_plus_sign: add socket io

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
-const router = require('./routes/index.routes.js')
+const cors = require('cors')
 
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
-
 app.set('PORT', process.env.PORT || 5000)
-app.use(router)
 
-app.listen(app.get('PORT'), () => {
+const server = app.listen(app.get('PORT'), () => {
   console.log('Server init on PORT', app.get('PORT'))
+})
+
+const io = require('socket.io')(server,{
+  cors:{
+    origin: process.env.SOCKET_CLIENT_URL,
+    methods:["POST, GET"]
+  }
+})
+
+io.on('connection', (socket)=>{
+  console.log('new connection')
+  socket.on('60ee46c949cde3054cc0523a', (data)=> console.log(data))
 })
